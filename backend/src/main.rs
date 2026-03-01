487e37cd3e33792b15e4f8e3ff4e31fbee390ca8
nakayesaa <hajendraherlambang742@gmail.com>
2026-03-01 23:27:30 +0700

    define price engine

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,20 @@
+use std::sync::{Arc, Mutex};
+
 mod config;
 mod models;
 mod coingecko;
 mod price_engine;
 mod ws_server;
 use config::Config;
+use price_engine::Pricestore;
 
-fn main(){
+#[tokio::main]
+async fn main(){
     tracing_subscriber::fmt().with_env_filter("info").init();
     let config = Config::from_env().expect("Load Config Failed");
     tracing::info!("Load Config : {:?}", config);
+
+    let price_store = Arc::new(Mutex::new(Pricestore::new()));
+    let price_store_coingecko = Arc::clone(&price_store);
+    let price_store_broadcast = Arc::clone(&price_store);
 }
--- backend/src/price_engine.rs
+++ backend/src/price_engine.rs
@@ -1,1 +1,41 @@
+use std::alloc::System;
 // TODO: implement
+use std::collections::{HashMap, VecDeque};
+use chrono::{Utc, Duration};
+use crate::models::PriceTick;
+
+pub struct Pricestore{
+    price_store : HashMap<String, VecDeque<PriceTick>>
+}
+
+impl Pricestore{
+    pub fn new() -> Self{
+        Self{
+            price_store : HashMap::new()
+        }
+    }
+
+    pub fn push_tick(&mut self, tick:PriceTick){
+        let deque = self
+            .price_store
+            .entry(tick.symbol.clone())
+            .or_insert_with(VecDeque::new);
+        
+        deque.push_back(tick);
+        let cut = Utc::now() - Duration::hours(24);
+
+        while let Some(front) = deque.front() {
+            if front.timestamp < cut{
+                deque.pop_front();
+            }else{
+                break;
+            } 
+                
+        }
+    }
+}
+
+
+
+
+
